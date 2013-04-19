ee8e560798497001a7dc8fd6fac0b2fe460bd764
Lars.im.Netz <java4fun@kaffeeschluerfer.com>
2013-04-19 19:56:33 +0200

    add Locale

--- src/main/java/org/homelinux/moonserver/WicketApplication.java
+++ src/main/java/org/homelinux/moonserver/WicketApplication.java
@@ -1,7 +1,12 @@
 package org.homelinux.moonserver;
 
+import java.util.Locale;
+
+import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -28,5 +33,16 @@ public class WicketApplication extends WebApplication
 		super.init();
 
 		// add your configuration here
+	}
+	
+	/**
+	 * Mal auf Deutsch einstellen
+	 */
+	@Override
+	public Session newSession(Request request, Response response) {
+		// TODO Auto-generated method stub
+		 Session newSession = super.newSession(request, response);
+		 newSession.setLocale(Locale.GERMANY);
+		 return newSession;
 	}
 }
