e6b44164ad22f5fc57b98103704eebbfd2e0496d
Lars Langhans <lars.langhans@hypoport.de>
2013-06-14 16:09:29 +0200

    deliver script

---
+++ deliver.sh
@@ -0,0 +1,2 @@
+# scp target/dateviewer.war vgl_allgemein@solv151.hypoport.local:/opt/instances/vgl_allgemein/webapps
+scp target/dateviewer.war vgl_bv@solv168.hypoport.local:/opt/instances/vgl_bv/webapps
