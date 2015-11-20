16c6e98373a2c0aad9f850c9a91a25ccc7d4fd81
Lars Langhans <lars.langhans@hypoport.de>
2015-11-20 08:28:21 +0100

    Ideen für ein neues einfacheres deliver Script

--- deliver.sh
+++ deliver.sh
@@ -1,19 +1,29 @@
-# scp target/dateviewer.war vgl_allgemein@solv151.hypoport.local:/opt/instances/vgl_allgemein/webapps
-# scp target/dateviewer.war vgl_bv@solv168.hypoport.local:/opt/instances/vgl_bv/webapps
+#!/bin/bash
+# -*- script -*-
+#
+# Deliver Script
+#
 
 FINDMOON=$(L findmoon)
+if [ -z "$FINDMOON" ]; then
+    echo "Moonserver nicht gefunden. Abbruch!"
+    exit 1
+fi
+
 WARFILE=$(L warfile)
 if [ -z "$WARFILE" ]; then
     echo "Kein WAR File gefunden, nichts gebaut?"
     exit 1
 fi
 
+# NICE TO HAVE: L deliver $WARFILE to $FINDMOON with $USER
+
 USER=langha_l
 PATHNAME=/var/lib/tomcat-7-testing/webapps
-SSH_OPT=
+SSH_OPT='-p 21007'
 
 DEBUGGING=
 
-$DEBUGGING ssh $SSH_OPT $USER@$FINDMOON  -p 21007 "sudo /etc/init.d/tomcat-7-testing stop"
-$DEBUGGING scp  -P 21007 ${WARFILE} $USER@${FINDMOON}:$PATHNAME
-$DEBUGGING ssh $SSH_OPT $USER@$FINDMOON  -p 21007 "sudo /etc/init.d/tomcat-7-testing start"
+$DEBUGGING ssh $SSH_OPT $USER@$FINDMOON "sudo /etc/init.d/tomcat-7-testing stop"
+$DEBUGGING scp $SSH_OPT ${WARFILE} $USER@${FINDMOON}:$PATHNAME
+$DEBUGGING ssh $SSH_OPT $USER@$FINDMOON "sudo /etc/init.d/tomcat-7-testing start"
